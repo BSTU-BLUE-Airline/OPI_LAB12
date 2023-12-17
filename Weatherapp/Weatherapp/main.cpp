8b682b47ce6ad08beee761d069f0112faf9a2b94
Anton <antonlutski@gmail.com>
2023-12-17 18:23:21 +0300

    Исправил приложение,работает пока только с английским языком

--- Weatherapp/Weatherapp/main.cpp
+++ Weatherapp/Weatherapp/main.cpp
@@ -1,60 +1,40 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <locale>
-#include <algorithm>
-#include <windows.h>
 
-std::wstring toLowerCase(const std::wstring& wstr) {
-    std::wstring lowerStr = wstr;
-    std::transform(lowerStr.begin(), lowerStr.end(), lowerStr.begin(), ::towlower);
-    return lowerStr;
-}
-  
 int main() {
     setlocale(LC_ALL, "Russian");
-    SetConsoleCP(1251);
 
-    std::wifstream inputFile("text.txt");
+    // Пользователь вводит город с клавиатуры
+    std::cout << "Введите название города: ";
+    std::string userCity;
+    std::cin >> userCity;
 
-    if (!inputFile.is_open()) {
-        std::cerr << "Не удалось открыть файл для чтения." << std::endl;
-        return 1;
-    }
+    // Открываем файл для чтения
+    std::ifstream inputFile("text.txt");
 
-    std::wcout << L"Введите название города: ";
-    std::wstring userInput;
-    std::getline(std::wcin, userInput);
+    // Проверяем, успешно ли открыт файл
+    if (inputFile.is_open()) {
+        std::string city, temperature;
 
-    std::wstring userInputLower = toLowerCase(userInput);
-
-    std::wstring line;
-    bool cityFound = false;
-
-    while (std::getline(inputFile, line)) {
-        size_t pos = line.find(L':');
-        if (pos != std::wstring::npos) {
-            std::wstring city = line.substr(0, pos);
-            std::wstring temperature = line.substr(pos + 1);
-
-            std::wstring cityLower = toLowerCase(city);
-
-            // Добавим сообщение для отладки
-            std::wcout << L"Из файла: Город - " << city << L", Температура - " << temperature << std::endl;
-
-            if (cityLower == userInputLower) {
-                std::wcout << L"Температура в городе " << city << L": " << temperature << std::endl;
-                cityFound = true;
-                break;
+        // Читаем файл построчно и ищем город
+        while (inputFile >> city >> temperature) {
+            // Если город найден, выводим информацию
+            if (city == userCity) {
+                std::cout << "Текущая температура в городе " << city << ": " << temperature << std::endl;
+                return 0;  // Завершаем программу после вывода информации
             }
         }
-    }
 
-    if (!cityFound) {
-        std::wcout << L"Город не найден." << std::endl;
-    }
+        // Если город не найден, выводим сообщение
+        std::cout << "Информация о погоде в городе " << userCity << " не найдена." << std::endl;
 
-    inputFile.close();
+        // Закрываем файл после завершения чтения
+        inputFile.close();
+    }
+    else {
+        std::cerr << "Не удалось открыть файл для чтения." << std::endl;
+    }
 
     return 0;
 }
