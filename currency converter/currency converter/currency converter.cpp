406d8d96b41e5f1d1d3ecc91737126ff69da195d
pruseezy <is5814221@gmail.com>
2023-12-20 02:08:24 +0300

    Пожилой лифт v2.0(не видит файл как будто)

--- currency converter/currency converter/currency converter.cpp
+++ currency converter/currency converter/currency converter.cpp
@@ -2,55 +2,45 @@
 #include <fstream>
 #include <sstream>
 #include <map>
-#include <iomanip>
+#include <string>
+
 using namespace std;
 
+struct CurrencyPair {
+    double buyRate;
+    double sellRate;
+};
+
 int main() {
-    ifstream file("text.txt");
-    if (!file.is_open()) {
-        cout << "Ошибка открытия файла!" << endl;
-        return 1;
-    }
+    setlocale(LC_ALL, "rus");
 
-    map<string, pair<double, double>> exchangeRates;
+    map<string, CurrencyPair> currencyPairs;
+
+    ifstream file("D:/lab12opi/OPI_LAB12/parser/text.txt");
     string line;
-
     while (getline(file, line)) {
-        stringstream ss(line);
-        string key;
+        istringstream iss(line);
+        string currencyPair;
+        getline(iss, currencyPair, '|');
         double buyRate, sellRate;
-        ss >> key >> buyRate >> sellRate;
-        exchangeRates[key] = make_pair(buyRate, sellRate);
-    }
-    file.close();
-
-    cout << "Доступные валюты:" << endl;
-    for (const auto& pair : exchangeRates) {
-        cout << pair.first << endl;
+        iss >> buyRate >> sellRate;
+        currencyPairs[currencyPair] = { buyRate, sellRate };
     }
 
-    string fromCurrency, toCurrency;
+    string from, to;
     double amount;
+    cout << "Введите исходную валюту, целевую валюту и сумму: ";
+    cin >> from >> to >> amount;
 
-    cout << "Введите исходную валюту (код валюты): ";
-    cin >> fromCurrency;
-    cout << "Введите желаемую валюту (код валюты): ";
-    cin >> toCurrency;
-    cout << "Введите сумму для конвертации: ";
-    cin >> amount;
-
-    string pairName = fromCurrency + " / " + toCurrency;
-    if (exchangeRates.find(pairName) == exchangeRates.end()) {
-        cout << "Ошибка: Такая пара валют не найдена." << endl;
+    string key = from + " / " + to;
+    if (currencyPairs.count(key) == 0) {
+        cout << "Курс валют не найден.\n";
         return 1;
     }
 
-    double buyRate = exchangeRates[pairName].first;
-    double sellRate = exchangeRates[pairName].second;
-    double result = amount * (buyRate + sellRate) / 2;
-
-    cout << fixed << setprecision(2);
-    cout << amount << " " << fromCurrency << " = " << result << " " << toCurrency << endl;
+    double result = amount * currencyPairs[key].sellRate;
+    cout << "Результат конвертации: " << result << ' ' << to << '\n';
 
     return 0;
 }
+
